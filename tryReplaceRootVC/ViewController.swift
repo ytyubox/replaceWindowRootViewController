802957781cd6ecbfb9075b15178c38b8cb403981
Tsungyu Yu <ytyubox@gmail.com>
2019-08-06 15:57:28 +0800

    project init

---
+++ tryReplaceRootVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryReplaceRootVC
+//
+//  Created by 游宗諭 on 2019/8/6.
+//  Copyright © 2019 ytyubox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
